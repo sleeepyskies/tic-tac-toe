b166c94aa75aa4b00cb98b4b30c45ffee4c42dec
Sky Waller <skyswalla@gmail.com>
2024-09-01 17:23:42 +0200

    Added printing board to console

--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 import sys
 
 # Initialises the game, explains the rules, and determines the gamemode
-def game_init():
+def game_init() -> None:
     print("Welcome to console Tic Tac Toe!")
     print("Enter 1 to play against another player, and 2 to play against the computer.")
 
@@ -13,13 +13,25 @@ def game_init():
             print("Playing against the computer!")
         case _:
             print("I'm sorry, you havent entered a valid input :(, exiting the game...)")
-            game_end()
+            game_end(0)
 
-def game_over(board):
+# Prints the current game board to the command line
+def print_board(board: list[str], current_round: int) -> None:
+    for i in range(3):
+        string_builder : str = "|"
+        for j in range(3):
+            string_builder += str(board[i*3 + j]) + "|"
+        print(string_builder)
+
+
+def game_over(board: list[str]) -> bool:
     return True
 
 # Prints the win status and exits the game
-def game_end(round):
-    message : str = "X has won the game in " + str(round) + " rounds!"
-    print(message)
+def game_end(round: int) -> None:
+    # If round is 0, then user has enetered incorrect input when deciding the game mode.
+    if (round != 0):
+        message : str = "X has won the game in " + str(round) + " rounds!"
+        print(message)
+
     sys.exit(0)
--- main.py
+++ main.py
@@ -7,12 +7,16 @@ game_active : bool = True
 # Determines current turn
 current_turn : int = 0
 # Holds the board data
-game_board : list[int] = [" " for _ in range(9)]
+game_board : list[str] = [" " for _ in range(9)]
 
 game_init()
 
 # Main game loop
 while(game_active):
+    # Print the current game board to console
+    print_board(game_board, current_turn)
+
+
     current_turn += 1
     game_active = not(game_over(game_board))
 
