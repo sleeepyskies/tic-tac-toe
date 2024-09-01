9c73833c1e3866f6235056428008777b541b5cfc
Sky Waller <skyswalla@gmail.com>
2024-09-01 17:02:13 +0200

    Added placeholder game start and end dialogue

--- helpers.py
+++ helpers.py
@@ -1,10 +1,25 @@
-# Initialises the game and explains the rules
+import sys
+
+# Initialises the game, explains the rules, and determines the gamemode
 def game_init():
     print("Welcome to console Tic Tac Toe!")
-    print("Hi!")
+    print("Enter 1 to play against another player, and 2 to play against the computer.")
+
+    gamemode = input()
+    match gamemode:
+        case "1":
+            print("Playing against another player!")
+        case "2":
+            print("Playing against the computer!")
+        case _:
+            print("I'm sorry, you havent entered a valid input :(, exiting the game...)")
+            game_end()
 
 def game_over(board):
     return True
 
-def game_end():
-    pass
+# Prints the win status and exits the game
+def game_end(round):
+    message : str = "X has won the game in " + str(round) + " rounds!"
+    print(message)
+    sys.exit(0)
--- main.py
+++ main.py
@@ -3,20 +3,17 @@ from helpers import *
 import sys
 
 # Used to check if game is still active
-game_active = True
+game_active : bool = True
 # Determines current turn
-current_turn = 0
+current_turn : int = 0
 # Holds the board data
-game_board = [" " for _ in range(9)]
+game_board : list[int] = [" " for _ in range(9)]
 
 game_init()
 
 # Main game loop
 while(game_active):
+    current_turn += 1
+    game_active = not(game_over(game_board))
 
-
-
-    game_active = game_over(game_board)
-
-game_end()
-sys.exit(0)
+game_end(current_turn)
