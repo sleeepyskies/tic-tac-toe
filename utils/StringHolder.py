cd281d536c6fbc1a75f8463756768793d4882640
Sky Waller <skyswalla@gmail.com>
2024-09-02 11:42:38 +0200

    Added utils module.

---
+++ utils/StringHolder.py
@@ -0,0 +1,12 @@
+class StringHolder:
+    """
+    Class used to store any string constants used in the game.
+    """
+
+    INVALID_INPUT = "I'm sorry, this input is invalid, please try again."
+
+    WELCOME_MESSAGE = "Welcome to console Tic-Tac-Toe!"
+    GAMEMODE_MESSAGE = "Enter 1 to play against another player, and 2 to play against the computer.\n"
+    
+    PLAYER_MODE = "Playing against another player!\n"
+    COMPUTER_MODE = "Playing against the computer!\n"
